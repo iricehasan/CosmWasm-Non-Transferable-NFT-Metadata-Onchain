a90f6d39598fb32c04548535916f6fc20020767e
iricehasan <hsn.irice@gmail.com>
2023-09-17 00:07:00 +0300

    Non-transferable NFT with metadata-onchain

--- src/state.rs
+++ src/state.rs
@@ -1,16 +1,10 @@
 use cosmwasm_schema::cw_serde;
-use cw_storage_plus::Item;
-
 use cosmwasm_std::Addr;
-
-use cw721_base::Extension;
+use cw_storage_plus::Item;
 
 #[cw_serde]
 pub struct Config {
-    pub cw721_address: Option<Addr>,
-    pub token_id: u64,
-    pub token_uri: String,
-    pub extension: Extension,
+    pub admin: Option<Addr>,
 }
 
 pub const CONFIG: Item<Config> = Item::new("config");
